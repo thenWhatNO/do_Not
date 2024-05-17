430d267b35ba2492c2da92d82aa831dcca871998
thenWhatNO <167019139+thenWhatNO@users.noreply.github.com>
2024-05-17 23:43:55 +0300

    Update test6.py

--- test6.py
+++ test6.py
@@ -1,17 +1,19 @@
 import numpy as np
 
 input = [0.2,0.4,0.6]
-W = [0.3,0.6,0.4]
+W = [[0.3,0.6,0.4],
+     [0.2,0.5,0.2]]
 bias = 3
 
-output = input[0] * W[0] + input[1] * W[1] + input[2] * W[2] + bias
-print(output)
+class NN:
+    def __init__(self, inp, nnn):
+        self.W1 = 0.10 * np.random.randn(inp, nnn)
+        self.bais = np.zeros(1, nnn)
+    def farward(self, inputs):
+        self.output = np.dot(input, self.W1) + self.bais
+        
+NN1 = NN(3, 6)
+NN2 = NN(6, 2)
 
-e = output - 0.8
-
-bias -= e
-
-print(W)
-
-output = input[0] * W[0] + input[1] * W[1] + input[2] * W[2] + bias
-print(output)
+NN1.farward(input)
+print(NN1.output)
