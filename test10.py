0e1f5ae5d66d6ce56306b6ab0ae9fb3a58e268f5
thenWhatNO <167019139+thenWhatNO@users.noreply.github.com>
2024-06-17 05:56:21 +0300

    up

--- test10.py
+++ test10.py
@@ -1,29 +1,51 @@
-import torch
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
-import pandas as pd
 
-class data:
-    def __init__(self, data_dir, bach_num):
-        self.banch = []
-        self.data_image = data_dir
-        df = pd.read_csv(self.data_image)
-        images, classes = df['image'], df['targ']
+data = {
+    0 : [[5,8],[10,6],[8,18],[20,12],[18,15],[13,5],[14,17],[20,13],[16,17],[19,7],[5,14]],
+    1 : [[10,10],[11,10],[10,12],[12,12],[13,11],[12,13],[11,14],[11,12],[11,11],[10,11],[9,13]]
+}
 
-        for i in range(bach_num):
-            r = np.random.randint(0,len(images))
-            img = Image.open('data' + images[r]).convert('L')
-            rec_img = img.resize((120,120))
-            img_arr = np.array(rec_img) 
-            self.bin_img = np.where(img_arr > 50, 0, 100)
-            self.banch.append([self.bin_img, classes[r]])
+colors = ['red', 'blue']
 
-    def get_banch(self):
-        return self.banch
+for i, points in data.items():
+    x = [point[0] for point in points]
+    y = [point[1] for point in points]
+    plt.scatter(x, y , color=colors[i])
 
-    def show(self):
-        plt.imshow(self.bin_img)
-        plt.axis('off')
-        plt.show()
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend()
+#plt.show()
 
+class NN:
+    def __init__(self, input, output):
+        self.layars = [
+            0.10 * np.random.randn(input, 2),
+            0.10 * np.random.randn(2,output)
+        ]
+    
+    def farward(self, datain):
+        self.datas = datain
+        self.out1 = np.dot(datain, self.layars[0])
+        self.out2 = np.dot(self.out1, self.layars[1])
+        return self.out2
+    
+    def optim(self, target, lr=0.1):
+        loss = target - self.out2
+        grad2 = loss * self.out1
+
+        grad1 = grad2 * self.datas
+
+        print(f"shapes grad2: {np.shape(self.layars[1])} grad1: {np.shape([grad2 * lr * self.out1])}")
+
+        self.layars[1] += [grad2 * lr * self.out1]
+        self.layars[0] += grad1 * lr * self.datas
+
+nn = NN(2, 1)
+hat_y = nn.farward(data[0][0])
+print(hat_y)
+nn.optim(0)
+
+hat_y = nn.farward(data[0][0])
+print(hat_y)
--- test9.py
+++ test9.py
@@ -21,18 +21,25 @@ class data:
     def get_banch(self):
         return self.banch
 
-    def show(self):
-        plt.imshow(self.bin_img)
+    def show(self, imag):
+        plt.imshow(imag)
         plt.axis('off')
         plt.show()
 
 class NN:
     def __init__(self, in_numbers, out_nambers):
+        self.m3 = np.zeros((50, out_nambers))
+        self.v3 = np.zeros((50, out_nambers))
+        self.m2 = np.zeros((100, 50))
+        self.v2 = np.zeros((100, 50))
+        self.m1 = np.zeros((in_numbers, 100))
+        self.v1 = np.zeros((in_numbers, 100))
+
         self.classes_num = out_nambers
         self.layers_NN = [
             [0.10 * np.random.rand(in_numbers, 100), np.zeros((1, 100))],
-            [0.10 * np.random.rand(100, in_numbers), np.zeros((1,in_numbers))],
-            [0.10 * np.random.rand(in_numbers, out_nambers), np.zeros((1, out_nambers))]
+            [0.10 * np.random.rand(100, 50), np.zeros((1,50))],
+            [0.10 * np.random.rand(50, out_nambers), np.zeros((1, out_nambers))]
         ]
 
     def Farward(self,batch_get, targ):
@@ -42,33 +49,44 @@ class NN:
         self.r_out1 = self.Relu(self.out1)
         self.out2 = np.dot(self.r_out1, self.layers_NN[1][0]) + self.layers_NN[1][1]
         self.r_out2 = self.Relu(self.out2)
-        self.out3 = np.dot(self.r_out2.T, self.layers_NN[2][0]) + self.layers_NN[2][1]
-        print(self.out3)
+        self.out3 = np.dot(self.r_out2, self.layers_NN[2][0]) + self.layers_NN[2][1]
+        print(f"output : {self.out3}")
     
     def Loss(self):
-        pass
+        return (self.out3 - self.targ)
 
-    def optim(self): 
-        self.labels = []
-        if self.targ == 0:
-            self.labels = [0,1]
-        elif self.targ == 1:
-            self.labels = [1,0]
+    def optim(self, lerning_r = 0.001, beta1 = 0.9, beta2 = 0.999, epsilon=1e-8, t=1): 
 
-        loss = (self.out3 - self.targ) / np.size(self.targ)
-        W3 = np.dot(self.r_out2.T, loss)
+        loss = (self.out3 - self.targ)
+        self.m3 = beta1 * self.m3 + (1-beta1) * np.array(loss)
+        self.v3 = beta2 * self.v3 + (1-beta2) * (np.array(loss)**2)
+        hat_m3 = self.m3 / (1-beta1 ** t)
+        hat_v3 = self.v3 / (1-beta2 ** t)
+        b3 = np.sum(loss, axis=0, keepdims=True)
 
         da2 = np.dot(loss, self.layers_NN[2][0].T)
         dz2 = da2 * self.relu_der(self.out2)
-        W2 = np.dot(self.r_out1.T, dz2)
+        self.m2 = beta1 * self.m2 + (1-beta1) * np.array(dz2)
+        self.v2 = beta2 * self.v2 + (1-beta2) * (np.array(dz2)**2)
+        hat_m2 = self.m2 / (1-beta1 ** t)
+        hat_v2 = self.v2 / (1-beta2 ** t)
+        b2 = np.sum(dz2, axis=0, keepdims=True)
 
         da1 = np.dot(dz2, self.layers_NN[1][0].T)
         dz1 = da1 * self.relu_der(self.out1)
-        W1 = np.dot(self.batch, dz1)
+        self.m1 = beta1 * self.m1 + (1-beta1) * np.array(dz1)
+        self.v1 = beta2 * self.v1 + (1-beta2) * (np.array(dz1)**2)
+        hat_m1 = self.m1 / (1-beta1 ** t)
+        hat_v1 = self.v1 / (1-beta2 ** t)
+        b1 = np.sum(dz1, axis=0, keepdims=True)
 
-        self.layers_NN[2][0] += 0.001 * W3
-        self.layers_NN[1][0] += 0.001 * W2
-        self.layers_NN[0][0] += 0.001 * W1
+
+        self.layers_NN[2][0] -= lerning_r * hat_m3 / (np.sqrt(hat_v3)+epsilon)
+        self.layers_NN[2][1] -= lerning_r * b3
+        self.layers_NN[1][0] -= lerning_r * hat_m2 / (np.sqrt(hat_v2)+epsilon)
+        self.layers_NN[1][1] -= lerning_r * b2  
+        self.layers_NN[0][0] -= lerning_r * hat_m1 / (np.sqrt(hat_v1)+epsilon)
+        self.layers_NN[0][1] -= lerning_r * b1
 
     #sub layers
     def Relu(self, x):
@@ -77,16 +95,20 @@ class NN:
     def relu_der(self, x):
         return np.where(x > 0,1,0)
 
-    def f(self, x):
-        return 2/(1 + np.exp(-x)) - 1
+    def softmax(self, x):
+        exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
+        
 
 images = data("plenet_data.csv", 100)
 banches = images.get_banch()
-nn = NN(120, 2)
+nn = NN(120*120, 1)
 
-
-print(f"target {banches[0][1]}")
-nn.Farward(banches[0][0], banches[0][1])
-nn.optim()
-print(f"target {banches[0][1]}")
-nn.Farward(banches[0][0], banches[0][1])
+for i in range(100):
+    img = banches[i][0].reshape(1, -1)
+    imgs = banches[i][0]
+    target = banches[i][1]
+    print(f"target {target} / ")
+    nn.Farward(img, target)
+    print(f"loss {nn.Loss()}")
+    nn.optim(t=i+1)
