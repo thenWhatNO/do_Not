8d05996f2b061c866ca00e103bd663932e8268f3
thenWhatNO <167019139+thenWhatNO@users.noreply.github.com>
2024-06-23 11:43:06 +0300

    up

--- test11.py
+++ test11.py
@@ -1,4 +1,20 @@
 import numpy as np
+import matplotlib.pylab as plt
+
+colors_plt = ['red', 'green']
+
+data = {
+    0 : [[1,2], [2,3], [2,4]],
+    1 : [[3,2], [3,1], [5,3]]
+}
+
+data1 = [0.1,0.2,0.3,0.4,0.5,0.6,0.7]
+
+np.random.seed(0)
+data_x = 2 * np.random.rand(100, 1)
+data_y = 4 + 3 * data_x + np.random.randn(100,1)
+
+#plt.scatter(data_x, data_y)
 
 input = np.array([1.0,5.0])
 hidden = np.array(
@@ -11,24 +27,39 @@ hidden2 = np.array([
 ])
 output = np.array([0.1, 0.2])
 
-def softmax(x, beta=1.0):
-    np.exp(beta * x) / np.sum(np.exp(beta * x))
+def sigmoid(x):
+    beta = 1.0
+    return 1 / (1 + np.exp(-x))
 
-sofmax_put = softmax(input)
-print(f"softmax {sofmax_put}")
+loss_plot = []
+i_plot = []
 
-for i in range(1):
-    out1 = np.dot(input, hidden.T)
+
+for i in range(300):
+    R_label = np.random.randint(0,2)
+    R_value = np.random.randint(0,3)
+
+    out1 = np.dot(data[R_label][R_value], hidden.T)
     out2 = np.dot(out1, hidden2.T)
     out3 = np.dot(out2, output)
 
-    loss = 2-out3
+    loss = np.abs(R_label-out3)
     gradient3 = np.dot(loss, out3)
-    gradient2 = np.dot(hidden2, loss)
-    gradient1 = np.dot(hidden, loss)
+    gradient2 = np.dot(loss, hidden2)
+    gradient1 = np.dot(loss, hidden)
 
-    output += 0.01 * gradient3
-    hidden2 += 0.01 * gradient2
-    hidden += 0.01 * gradient1 
+    output -= 0.00001 * gradient3 * out2
+    hidden2 -= 0.00001 * gradient2 * out1
+    hidden -= 0.00001 * gradient1 * data[R_label][R_value]
+    print(f" loss1 {loss}, label {R_label}, output: {out3}")
 
-    print(f"target 2, loss1 {loss}, output: {out3}")
+    loss_plot.append(loss)
+    i_plot.append(i)
+    
+    plt.scatter(i, out3, color=colors_plt[R_label])
+
+    plt.scatter(out3, out3, color='green')
+
+    
+plt.plot(i_plot, loss_plot, color='orange')
+plt.show()
--- test7.py
+++ test7.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 input = [[1,0,0],[1,1,0],[1,1,1]]
 W = [0.3,0.3,0.3]
@@ -7,24 +8,34 @@ bias = 1
 
 targ = [0.1, 0.5, 1]
 
-for i in range(30):
+loss_plot = []
+i_plot = []
+
+
+for i in range(300):
+
     R = np.random.randint(0,3)
 
     y = np.dot(input[R], W)
     
-    loss = targ[R] - y
+    loss = np.abs(targ[R] - y)
     
     gradient = []
-    for i in range(3):
-        o = targ[R] - np.abs(input[R][i])
+    for ii in range(3):
+        o = targ[R] - np.abs(input[R][ii])
         gradient.append(o)
     
     gradient2 = np.dot(loss,input[R])
 
-    bias = bias + 0.01 * loss
+    bias = bias + 0.001 * loss
 
-    print(f"grad 1 : {gradient} grad 2 {gradient2}")
-
-    for i in range(3):
-        W[i] = W[i] + 0.01 * gradient[i] * input[R][i]
+    for r in range(3):
+        W[r] = W[r] + 0.001 * gradient[r] * input[R][r]
     print(f"loss : {loss}, target : {targ[R]}, output N : {y}")
+
+    loss_plot.append(loss)
+    i_plot.append(i)
+    plt.scatter(i, y, color='red')
+
+plt.plot(i_plot, loss_plot, color='orange')
+plt.show()
