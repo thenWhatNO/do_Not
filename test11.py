637a3178d68ecc665f5f528d40dabe087641a91b
thenWhatNO <167019139+thenWhatNO@users.noreply.github.com>
2024-07-08 20:59:53 +0300

    Update test11.py

--- test11.py
+++ test11.py
@@ -1,16 +1,17 @@
 import numpy as np
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 
 np.random.seed(0)
-data_x = 3 * np.random.rand(50, 1)
-data_y = 1 + data_x**4 + np.random.rand(50, 1)**2
+data_x = np.array(3 * np.random.rand(50, 1))
+data_y = np.array(1 + data_x**4 + np.random.rand(50, 1)**2)
 
-W1 = np.array([0.1])
-b1 = np.array([0.1])
-W2 = np.array([[0.1],[0.1]])
-b2 = np.array([0.1,0.1])
-outW = np.array([0.1,0.1])
-outb = np.array([0.1])
+#plt.scatter(data_x, data_y)
+
+W1 = np.random.rand(10,1)
+b1 = np.random.rand(10)
+
+outW = np.random.rand(1,10)
+outb = np.random.rand(1)
 
 def relu(X):
     return np.maximum(0, X)
@@ -18,22 +19,58 @@ def relu(X):
 def derv_relu(x):
     return np.where(x > 0,1,0)
 
-def farword(w1,b1,w2,b2,outw,outb, input):
-    layar_out1 = np.dot(input, w1) + b1
-    activ_func1 = relu(layar_out1)
-    layar_out2 = np.dot(activ_func1, w2.T) + b2
-    activ_func2 = relu(layar_out2)
-    output_layer = np.dot(activ_func2, outw) + outb
-    return activ_func1, activ_func2, output_layer
+I_arr = []
+YS_arr = []
 
-def optim(y, ln=0.01):
-    out1, out2, y_hat = farword(W1,b1,W2,b2,outW,outb,data_x[0][0])
-    error = y - y_hat
-    out_grad = error * derv_relu(y)
-    W2_grad = outW * out_grad * derv_relu(out2)
-    W1_grad = W2 * W2_grad * derv_relu(out1)
-    grad = W1 * W1_grad * derv_relu(data_x[0][0])
 
-    print(f"out_grad {out_grad}, W2_grad { W2_grad}, W1_grad{W1_grad}, grad {grad}")
+for i in range(4000):
 
-optim(data_y[0][0])
+    R = np.random.randint(0, len(data_x))
+    Y = data_y[R][0]
+    steps = data_x[R][0]
+
+    Z = np.dot(steps, W1[0]) + b1
+    A = np.array([relu(Z)])
+
+    ZZ = np.dot(A, outW.T) + outb
+    AA = relu(ZZ)
+
+    DA = -(2 / data_x.shape[0]) * (Y - AA)
+    
+    DZ = DA * derv_relu(AA)
+    DW = np.dot(A.T, DZ)
+    DB = np.sum(DZ, axis=0, keepdims=True)
+
+    DA2 = np.dot(DZ, outW)
+    DZ2 = DA2 * derv_relu(A)
+    DW2 = np.dot(steps, DZ)
+    DB2 = np.sum(DZ2, axis=0, keepdims=True)
+
+    outW -= 0.01 * DW.T
+    outb -= 0.01 * DB[0]
+
+    W1 -= 0.01 * DW2
+    b1 -= 0.01 * DB2[0]
+    print(f"Y : {AA} Y_HAT : {Y} LOSS : {DA[0][0]}")
+    
+    I_arr.append(i)
+    YS_arr.append(DA[0][0])
+
+    #plt.scatter(steps, AA, color="red")  
+
+
+
+for i in range(50):
+    Z = np.dot(data_x[i], W1[0]) + b1
+    A = np.array([relu(Z)])
+
+    ZZ = np.dot(A, outW.T) + outb
+    AA = relu(ZZ)
+    plt.scatter(data_x[i], AA, color="red")        
+plt.show()
+
+print("?")
+
+plt.scatter(I_arr, YS_arr, color="green")
+plt.grid()
+plt.show()
