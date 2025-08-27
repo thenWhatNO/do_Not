0a919242f5756e5d819b9be4386537316c44b701
thenWhatNO <167019139+thenWhatNO@users.noreply.github.com>
2025-08-27 17:49:22 +0300

    up

--- test28.py
+++ test28.py
@@ -2,69 +2,47 @@ x = [[1,2,3],
      [4,5,6,7],
      [7,8,9]]
 
-y = [[1,1,1],
-     [2,2,2],
-     [3,3,3]]
-
-show_len = len(x)
-
-
-def test_func(data):
-    shape = []
-    while isinstance(data, list):
-        shape.append(len(data))
-        data = data[0] if len(data) > 0 else []
-    return shape
-
-shapy = test_func(x)
-
-
-class Matrix:
-    def __init__(self, data):
-        self.data = data
-
-    class Vector:
-        def __init__(self, data):
-            self.vectors = self.split_data_to_vectors(data)
-            self.shape = self.shape_detect(data)
-            self.type = self.type_detect(data)
-
-        def shape_detect(data):
-            pass
-
-        def type_detect(data):
-            pass
-
-        def split_data_to_vectors(data):
-            shape = []
-
-            start_shape = len(data)
-            shape.append(start_shape)
+y = [[[1,2,3],
+     [4,5,6],
+     [7,8,9]]]
 
 
 
-        
-        def recursiv_split(data):
-            shapes = []
-            for i in data:
-                if len(i) not in shapes:
-                    shapes.append(len(i))
-            if len(shapes) > 1:
-                raise ValueError(f"the shape is ended as {shapes}, what is not functioanal for matrix useg")
-            else:
-                return data[0]
+class EliNum:
+
+    def __init__(self):
+        self.data = {}
+        self.shape = None
+
+def generait_data(data):
     
-    def __matmul__(self, other):
+    first_shape = len(data)
 
-        row_a = len(self.data)
-        col_a = len(self.data[0])
-        row_b = len(other.data)
-        col_b = len(other.data[0])
+    shape_of_array = [first_shape]
+    scan_box = data.copy()
+    shape_boxs = []
 
-        output = [[0 for _ in range(col_b)] for _ in range(row_a)]
+    shaps = []
 
-        for i in range(row_a):
-            for j in range(col_b):
-                for k in range(col_a):
-                    output[i][j] += self.data[i][k] * other.data[k][j]
+    run = True
+    while run:
 
+        try:
+            for layer in scan_box:
+                [shape_boxs.append(x) for x in layer]
+                shaps.append(len(layer))
+        except Exception as e:
+            run = False
+
+        if all(ind != shaps[0] for ind in shaps):
+            print("the shape is not the same!")
+            break
+        
+        shape_of_array.append(shaps[0])
+
+        scan_box = shape_boxs.copy()
+        shape_boxs = []
+    
+    return shape_of_array
+
+test = generait_data(y)
