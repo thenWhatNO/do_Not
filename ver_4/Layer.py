4a0f8fd6b4d698bb532d4f092000f690e0be6e56
thenWhatNO <167019139+thenWhatNO@users.noreply.github.com>
2025-09-06 10:01:15 +0300

    up

---
+++ ver_4/Layer.py
@@ -0,0 +1,156 @@
+import numpy as np
+
+
+#  regular data shape (batch, input_Z, output_Z)
+#  conv data shape (batch, W, H, Channer)
+#  tensers data shape (batch, num of word, tenser lenght)
+
+class Layer:
+    def __init__(self, inP, outP, active_func=None, optimazer=None, trainable=True, normalization=False, dropout=False):
+        #data memory
+        self.input_data = None
+        self.pre_activ_output = None
+        self.norm_data_output = None
+        self.activ_output = None
+        self.dropted_data_output = None
+
+        #norma outputs
+        self.mu = None
+        self.ver = None
+        self.hat_z = None
+
+        #parameter memory
+        self.in_shape = inP
+        self.out_shape = outP
+
+            #params
+        self.Wight = None
+        self.Bias = None
+        self.gamma = None
+        self.beta = None
+
+        self.dropout_mask = None
+
+        #gradient
+        self.D_input = None
+
+            #D params
+        self.D_Wight = None
+        self.D_Bias = None
+        self.D_gamma = None
+        self.D_beta = None
+
+        #functions
+        self.activation_func = active_func
+        self.optimazer = optimazer
+
+        #other
+        self.trainable = trainable
+        self.normaliz = normalization
+        self.drop_out = dropout
+
+    def Linear_prosecc(self, input):
+        return np.matmul(input, self.Wight) + self.Bias
+    
+    def Normalization(self, input, epsilon=1e-9):
+        self.mu = np.mean(input, axis=0)
+        self.ver = np.var(input, axis=0)
+
+        self.hat_z = (input - self.mu) / np.sqrt(self.ver + epsilon)
+
+        return (self.gamma * self.hat_z) + self.beta  
+
+    def dropout(self, input, p_):
+        if self.trainable:
+            mask = np.random.choice([0,1], input.shape, p=[p_, 1-p_]).astype(float)
+            output = (mask * input) / (1 - p_)
+            return output, mask
+        else:
+            return input, None
+    
+    def active_data(self, input, active_func):
+        return active_func.activ(input)
+
+    def build_layer(self, test_in):
+        self.Wight = np.random.randn(self.out_shape, self.in_shape) * np.sqrt(2. / self.in_shape)
+        self.Bias = np.random.randn(1, self.out_shape)
+
+        self.gamma = np.ones((1, self.out_shape))
+        self.beta = np.zeros((1, self.out_shape))
+
+    def farword(self, input_data):
+        self.input_data = input_data
+
+        out = self.Linear_prosecc(input_data)
+        self.pre_activ_output = out.copy()
+
+        if self.normaliz:
+            out = self.Normalization(out)
+            self.norm_data_output = out.copy()
+        
+        if self.activation_func != None:
+            out = self.active_data(out)
+            self.activ_output = out.copy()
+
+        if self.drop_out:
+            out, mask = self.dropout(out)
+            self.dropted_data_output = out.copy()
+            self.dropout_mask = mask.copy()
+
+        return out
+    
+    def D_Linear_prosecc(self, D_input):
+        self.D_Wight = np.matmul(D_input, self.input_data)
+        self.D_Bias = np.sum(D_input, axis=0, keepdims=True)
+        return np.matmul(D_input, self.Wight.T)
+    
+    def D_active_data(self, D_input):
+        return D_input * self.activation_func.derev(self.pre_activ_output)
+
+    def D_Normalization(self, D_input, epsilon=1e-9):
+        N = D_input.shape[0]
+
+        # шаги как в теории
+        dx_hat = D_input * self.gamma
+
+        dvar = np.sum(dx_hat * (self.pre_activ_output - self.mu) * -0.5 * (self.ver + epsilon) ** (-1.5), axis=0)
+        dmu = np.sum(dx_hat * (-1 / np.sqrt(self.ver + epsilon)), axis=0) + dvar * np.mean(-2 * (self.pre_activ_output - self.mu), axis=0)
+
+        dx = dx_hat / np.sqrt(self.ver + epsilon) + dvar * 2 * (self.pre_activ_output - self.mu) / N + dmu / N
+
+        self.D_gamma = np.sum(D_input * self.hat_z, axis=0)
+        self.D_beta = np.sum(D_input, axis=0)
+
+        return dx
+    
+    def D_dropout(self, D_input):
+        return D_input * self.dropout_mask
+    
+    def backword(self, D_input):
+        D_inp = D_input
+
+        if self.drop_out:
+            D_inp= self.D_dropout(D_inp)
+
+        if self.activation_func != None:
+            D_inp = self.D_active_data(D_inp)
+
+        if self.normaliz:
+            D_inp = self.D_Normalization(D_inp)
+        
+        D_inp = self.Linear_prosecc(D_inp)
+
+        self.tune_Param()
+        return D_inp
+
+
+    ## to-do : add the learning param to the tuning parameters
+    def tune_Param(self):
+        if self.D_Wight != None:
+            self.optimazer.optim(self.Wight ,self.D_Wight)
+        if self.D_beta != None:
+            self.optimazer.optim(self.beta, self.D_beta)
+        if self.D_gamma != None:
+            self.optimazer.optim(self.gamma, self.D_gamma)
+        if self.D_beta != None:
+            self.optimazer.optim(self.beta, self.D_beta)
